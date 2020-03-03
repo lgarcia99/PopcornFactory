b830ba59ef9beca38e020630456e2b9d4ea82ffb
Luis García <luis_david_26g@hotmail.com>
2020-03-03 09:15:00 -0700

    first commit

---
+++ app/src/main/java/garcia/luis/popcornfactory/MovieDetail.kt
@@ -0,0 +1,24 @@
+package garcia.luis.popcornfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_movie_detail.*
+
+class MovieDetail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+
+        val bundle = intent.extras
+
+        if (bundle != null){
+
+
+            movie_header.setImageResource(bundle.getInt("header"))
+            movie_title_detail.text = bundle.getString("titulo")
+            movie_summary.text = bundle.getString("sinopsis")
+        }
+
+    }
+}
